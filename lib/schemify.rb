99a45ec5ee629264b1de6f57690ef150f175d0ef
Rylan Polster <rslpolster@gmail.com>
2025-06-15 14:55:02 -0400

    Initial commit

---
+++ lib/schemify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "schemify/version"
+
+module Schemify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/schemify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Schemify
+  VERSION = "0.1.0"
+end
